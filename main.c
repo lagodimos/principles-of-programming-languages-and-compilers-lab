0f6c85ba88bf443679ff9bbecf0d671ee83f34d0
Giannis Lagodimos <62063308+lagodimos@users.noreply.github.com>
2025-06-02 16:58:38 +0300

    Read from file

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include "parser.tab.h"
 
-int main() {
+extern FILE *yyin;
+
+int main(int argc, char **argv) {
+    FILE *input_file;
+
+    input_file = fopen(argv[1], "r");
+
+    if (input_file) {
+        yyin = input_file;
+    }
+    else {
+        printf("Unable to open %s.\n", argv[1]);
+        return -1;
+    }
+
     if (yyparse() == 0) {
         printf("Parsing completed successfully.\n");
     } else {
